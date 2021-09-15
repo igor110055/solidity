dcb33d7dcd8bcfec00326e8c56114dd51628e7e8
Moicky <michael.beihammer@gmail.com>
2021-09-15 19:27:45 +0200

    update

--- box/contracts/testSwap.sol
+++ box/contracts/testSwap.sol
@@ -1,23 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.5.0;
 
-import "./interfaces/IPancakeFactory.sol";
-import "./interfaces/IPancakePair.sol";
+//import "./interfaces/IPancakeFactory.sol";
+//import "./interfaces/IPancakePair.sol";
 
 contract TestSwapContract {
-    address private FACTORY;
+    address private constant FACTORY = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
 
     event Log(string message, uint value);
 
-    constructor(address factoryAddress) public {
-        emit Log("Constructor called", 1);
-        FACTORY = factoryAddress;
+    uint temp = 0;
+
+    function set(uint _data) public payable {
+        temp = _data;
+        emit Log("Value set:", temp);
     }
 
-    function swap(address token0, address token1, uint amount0) public {
-        address pair = IPancakeFactory(FACTORY).getPair(token0, token1);
-        uint amount0Out = token0 == IPancakePair(pair).token0() ? amount0 : 0;
-        uint amount1Out = token0 == IPancakePair(pair).token1() ? amount0 : 0;
-        emit Log("amount0Out", amount0Out);
-        emit Log("amount1Out", amount1Out);
+    function get() public view returns (uint){
+        return temp;
+    }
+
+    function swap(string memory something) public {
+        //address pair = IPancakeFactory(FACTORY).getPair(token0, token1);
+        //uint amount0Out = token0 == IPancakePair(pair).token0() ? amount0 : 0;
+        //uint amount1Out = token0 == IPancakePair(pair).token1() ? amount0 : 0;
+        emit Log("amount0Out", 1);
+        emit Log("amount1Out", 2);
+        emit Log(something, 3);
     }
 }
--- box/migrations/2_deploy_contracts.js
+++ box/migrations/2_deploy_contracts.js
@@ -1,9 +1,6 @@
-console.log("test")
-let testSwap = artifacts.require("TestSwapContract");
-let pancakeFactory = artifacts.require("PancakeFactory")
+const MyContract = artifacts.require("TestSwapContract");
 
-module.exports = function(deployer, network, accounts) {
-    console.log("Deploying Factory")
-    deployer.deploy(pancakeFactory)
-    console.log("Deployed Factory")
+module.exports = async function (deployer, network, accounts) {
+    await deployer.deploy(MyContract);
+    console.log("Done deploying")
 };
