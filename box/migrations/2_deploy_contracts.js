5d0028c96ca22137a624b435ff8b897f22efd93c
Moicky <michael.beihammer@gmail.com>
2021-09-16 17:11:52 +0200

    fixed .gitignore

--- box/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const MyContract = artifacts.require("TestSwapContract");
-
-module.exports = async function (deployer, network, accounts) {
-    await deployer.deploy(MyContract);
-    console.log("Done deploying")
-};
