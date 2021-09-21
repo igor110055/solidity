429ee0194c831b78e441f2ad2866837266a16841
Moicky <michael.beihammer@gmail.com>
2021-09-21 12:45:31 +0200

    pancake-pairs imported

--- Exchanges/Biswap/Biswap.js
+++ Exchanges/Biswap/Biswap.js
@@ -1,23 +1,20 @@
 const Exchange = require("../Exchange")
-const Web3 = require("web3")
 
 class Biswap extends Exchange {
-    constructor(web3URL) {
+    constructor(web3) {
         super();
         this.factoryABI = require("./ABIs/Factory.json")
         this.pairABI = require("./ABIs/Pair.json")
 
         this.factoryAddress = "0x858E3312ed3A876947EA49d572A7C42DE08af7EE"
 
-        this.web3URL = web3URL
-
-        this.web3 = new Web3(this.web3URL)
+        this.web3 = web3
         this.factoryContract = new this.web3.eth.Contract(this.factoryABI, this.factoryAddress)
     }
 
     async getReserves(addressIn, addressOut) {
         return new Promise(async (resolve, reject) => {
-            const pairAddress = await this.factoryContract.methods.getPair(addressOut, addressIn).call()
+            const pairAddress = await this.factoryContract.methods.getPair(addressIn, addressOut).call()
             if (pairAddress === "0x0000000000000000000000000000000000000000")
                 return reject("Not a valid pair")
 
@@ -36,9 +33,9 @@ class Biswap extends Exchange {
 
     async getAmountOut(amountIn, reserve0, reserve1, fee) {
         return new Promise(async (resolve, reject) => {
-            if (amountIn <= 0 || reserve0 <= 0 || reserve1 <= 0) {
+            if (amountIn <= 0 || reserve0 <= 0 || reserve1 <= 0)
                 return reject("At least 1 value is <= 0")
-            }
+
             let amountInWithFee = amountIn * (10000 - fee)
             let numerator = amountInWithFee * reserve1
             let denominator = reserve0 * 10000 + amountInWithFee
--- Exchanges/Exchange.js
+++ Exchanges/Exchange.js
@@ -1,8 +1,8 @@
 module.exports = class Exchange {
-    constructor(web3URL) {
+    constructor(web3) {
         if (new.target === Exchange)
             throw new Error("Class is abstract.")
-        this.web3URL = web3URL
+        this.web3 = web3
     }
 
     async getReserves(addressIn, addressOut) {
