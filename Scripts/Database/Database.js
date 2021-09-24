19ea856574189683f99fbc95d993ea23632d4939
Moicky <michael.beihammer@gmail.com>
2021-09-24 15:12:35 +0200

    finished database class

--- Scripts/Database/Database.js
+++ Scripts/Database/Database.js
@@ -1,35 +1,50 @@
-//const sqlite = require("sqlite3").verbose()
-//const db = new sqlite.Database("./database.db")
-
-// db.run("create table pairs (type text, hash text, token0 text, token1 text)")
-
-//const fs = require("fs")
-//const data = JSON.parse(fs.readFileSync("./Pancake/allKnownPairs.json").toString())
-//
-//for (const token in data) {
-//    db.run(`insert into pairs (hash, token0, token1) values ("${token}", "${token[2]}", "${token[3]}")`)
-//}
-
-//console.time("This is fast")
-//db.all("select hash from pairs where token0='e'", (err, row) => {
-//    if (err)
-//        console.log(err)
-//    else {
-//        console.log(row)
-//        console.timeEnd("This is fast")
-//    }
-//})
-
-class Database{
+module.exports = class Database {
     constructor() {
         this.fs = require("fs")
         this.config = JSON.parse(this.fs.readFileSync("./config.json").toString())
         this.saveConfig = () => this.fs.writeFileSync("./config.json", JSON.stringify(this.config, null, 3))
 
-        if(this.config["runSetup"]){
+        this.sqlite3 = require("sqlite3")
+        this.db = new this.sqlite3.Database(this.config["databaseDirectory"])
+
+        if (this.config["runSetup"]) {
             this.config["runSetup"] = false
+            this._createTables()
             this.saveConfig()
         }
     }
+
+    _createTables() {
+        for (const table in this.config["tables"]) {
+            let columns = []
+            for (const column in this.config["tables"][table])
+                columns.push(`${column} ${this.config["tables"][table][column]}`)
+
+            // @formatter:off
+            this.db.run(`create table ${table} (${columns.join(", ")})`)
+            // @formatter:on
+        }
+    }
+
+    saveData(table, jsonData) {
+        let values = []
+        for (const columnValue in jsonData)
+            values.push(`"${jsonData[columnValue]}"`)
+
+        // @formatter:off
+        let insertCommand = `insert into ${table} (${Object.keys(jsonData).join(", ")}) values (${values.join(", ")})`
+        // @formatter:on
+
+        this.db.run(insertCommand)
+    }
+
+    async getData(table, condition = "") {
+        return new Promise((resolve => {
+            // @formatter:off
+            this.db.all(`select * from ${table} ${condition}`, (err, res) => {
+            // @formatter:on
+                resolve(res)
+            })
+        }))
+    }
 }
-const test = new Database()
